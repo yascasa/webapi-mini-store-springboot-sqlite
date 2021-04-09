be890b11d576f068f8e222e8fbf36c909e869789
yassine <YAS@ze.com>
2021-04-09 23:11:06 +0100

    fix authentication login

--- src/main/java/com/pfejava/springbootpfe/common/AppMessages.java
+++ src/main/java/com/pfejava/springbootpfe/common/AppMessages.java
@@ -4,5 +4,5 @@ public class AppMessages {
 	public static final String AUTHENTICATION_FAILED="Le nom d'utilisateur ou mot de passe est incorrect";
 	public static final String INTERNAL_ERROR_SERVER="Une erreur interne s'est produite";
 	public static final String NOT_FOUND="Il n'y a pas de résultat pour votre recherche";
-	
+	public static final String EMAIL_USED="Cet email est déjà utilisé";
 }
--- src/main/java/com/pfejava/springbootpfe/security/JwtUserService.java
+++ src/main/java/com/pfejava/springbootpfe/security/JwtUserService.java
@@ -2,6 +2,7 @@ package com.pfejava.springbootpfe.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,10 +23,17 @@ public class JwtUserService implements UserDetailsService{
 	@Autowired
 	private UserRepository userRepository;
 	
-	@Override
-	public AppUser loadUserByUsername(String userName) throws UsernameNotFoundException {
-		AppUser user = this.userRepository.findByUserName(userName);
+	
+	public AppUser loadUserByUsernameAndPassword(String userName, String password) throws UsernameNotFoundException{
+		AppUser user = this.userRepository.getUserByUserName(userName, password);
 		return user;
+	}
+
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
